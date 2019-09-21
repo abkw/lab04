ef521e21db90ffa18de08fe6aff06f4abd7fbf34
Mohammed Bakheet <m.yousify@gmail.com>
2019-09-21 21:21:56 +0200

    updating linear.R

--- R/linreg.R
+++ R/linreg.R
@@ -1,14 +1,21 @@
 #This function uses RC class
-linear <- setRefClass("linear",
-                      fields = list(formula = "formula", data = "character"),
+linreg <- setRefClass("linear",
+                      fields = list(formula= "formula", data="data.frame"),
                       methods = list(
-                        linreg = function(formula, data) {
+                        linreg = function(formula = "formula", data="data.frame") {
+                          if (formula == !as.formula()){
+                            stop("formula has to be a formula")
+                          }
+                          if (data == !as.data.frame()){
+                            stop("data has to be a dataframe")
+                          }
+                          all.vars(formula, functions = FALSE, max.names = -1L, unique = TRUE)
                           obj <- linear$new()
                           obj <- model.matrix(data)
-                          return (obj)
+                          return (all.vars(obj, functions = FALSE, max.names = -1L, unique = TRUE))
                         }
                         )
 )
-x <- as.formula("y ~ x1 + x2")
-linregResult <- linear$new(formula =x,data = "ng")
-linregResult$linreg(formula = x,data = "ng")
+data("iris")
+linreg_mod <- linreg$new(formula = Petal.Length~Sepdsal.Width+Sepal.Length, data=iris)
+print(linreg_mod)
