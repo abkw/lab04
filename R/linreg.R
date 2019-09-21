4bb60b1afc440b2f99f50f34e92ed90b9efe2b65
Mohammed Bakheet <m.yousify@gmail.com>
2019-09-22 01:02:27 +0200

    linear with correct output

--- R/linreg.R
+++ R/linreg.R
@@ -2,20 +2,12 @@
 linreg <- setRefClass("linear",
                       fields = list(formula= "formula", data="data.frame"),
                       methods = list(
-                        linreg = function(formula = "formula", data="data.frame") {
-                          if (formula == !as.formula()){
-                            stop("formula has to be a formula")
-                          }
-                          if (data == !as.data.frame()){
-                            stop("data has to be a dataframe")
-                          }
-                          all.vars(formula, functions = FALSE, max.names = -1L, unique = TRUE)
-                          obj <- linear$new()
-                          obj <- model.matrix(data)
-                          return (all.vars(obj, functions = FALSE, max.names = -1L, unique = TRUE))
+                        linreg = function(formula = as.formula(), data=as.data.frame()) {
+                          mat <- model.matrix(formula,data)
+                          return (all.vars(mat))
                         }
                         )
 )
+
 data("iris")
-linreg_mod <- linreg$new(formula = Petal.Length~Sepdsal.Width+Sepal.Length, data=iris)
-print(linreg_mod)
+print(linreg_mod$linreg(formula = Petal.Length~Sepal.Width+Sepal.Length, data=iris))
