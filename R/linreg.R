d249306e9aa8a7f11f82fa500eddb1121b743f05
Mohammed Bakheet <m.yousify@gmail.com>
2019-09-22 02:05:11 +0200

    using lm function

--- R/linreg.R
+++ R/linreg.R
@@ -1,13 +1,15 @@
 #This function uses RC class
-linreg <- setRefClass("linear",
-                      fields = list(formula= "formula", data="data.frame"),
+linreg <- setRefClass("linreg", fields = list(formula= "formula", data="data.frame"),
                       methods = list(
-                        linreg = function(formula = as.formula(), data=as.data.frame()) {
+                        linreg = function(formula = as.formula(), data=as.data.frame(),...) {
                           mat <- model.matrix(formula,data)
-                          return (all.vars(mat))
+                          return (summary(lm(formula,data)))
+
                         }
                         )
 )
 
 data("iris")
+linreg_mod <- linreg$new()
 print(linreg_mod$linreg(formula = Petal.Length~Sepal.Width+Sepal.Length, data=iris))
+
