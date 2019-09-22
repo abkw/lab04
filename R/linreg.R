5d8e873d41fef4b9f3f424ca9a7886a12cdd4115
Mohammed Bakheet <m.yousify@gmail.com>
2019-09-22 04:03:39 +0200

    calculating Regressions coefficients

--- R/linreg.R
+++ R/linreg.R
@@ -2,14 +2,20 @@
 linreg <- setRefClass("linreg", fields = list(formula= "formula", data="data.frame"),
                       methods = list(
                         linreg = function(formula = as.formula(), data=as.data.frame(),...) {
+                          yName <- all.vars(formula)[1]
+                          y <- data[,as.character(yName)]
                           mat <- model.matrix(formula,data)
-                          return (summary(lm(formula,data)))
-
+                          mat <- cbind(mat,y)
+                          return(mat)
                         }
                         )
 )
 
 data("iris")
 linreg_mod <- linreg$new()
-print(linreg_mod$linreg(formula = Petal.Length~Sepal.Width+Sepal.Length, data=iris))
-
+theMatrix <- linreg_mod$linreg(formula = Petal.Length~Sepal.Width+Sepal.Length, data=iris)
+y <- theMatrix[,ncol(theMatrix)]
+theMatrix <- theMatrix[,-ncol(theMatrix)]
+matTranspose <- t(theMatrix)
+result <- ((matTranspose%*%theMatrix)^-1)%*%matTranspose%*%y
+print(result)
