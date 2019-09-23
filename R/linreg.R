fed55d3e311fa77703c3e1ccf668c50291789186
Mudith Chathuranga <mudithchathuranga@gmail.com>
2019-09-23 15:55:09 +0200

    Bug Fixing

--- R/linreg.R
+++ R/linreg.R
@@ -7,28 +7,44 @@ linreg <- setRefClass("linreg", fields = list(formula= "formula", data="data.fra
                           mat <- model.matrix(formula,data)
                           mat <- cbind(mat,y)
                           return(mat)
+                        },
+                        linreg1 = function(formula = as.formula, data=as.data.frame){
+                          mat <- model.matrix(formula,data)
+                          x <- lm(formula,data)
+                          return (summary(x))
                         }
                         )
 )
 
 data("iris")
 linreg_mod <- linreg$new()
+linreg_mod1<- linreg$new()
+
+linreg_mod1 <- linreg_mod$linreg1(formula = Petal.Length~Sepal.Width+Sepal.Length, data=iris)
+print(linreg_mod1$residuals)
+
 theMatrix <- linreg_mod$linreg(formula = Petal.Length~Sepal.Width+Sepal.Length, data=iris)
 y <- theMatrix[,ncol(theMatrix)]
-theMatrix <- theMatrix[,-ncol(theMatrix)]
+X <- theMatrix[,-ncol(theMatrix)]
 matTranspose <- t(theMatrix)
 #---------------------------------------------Regression Coefficients-------------------------------
-regressionsCoefficients <- ((matTranspose%*%theMatrix)^-1)%*%matTranspose%*%y
+# regressionsCoefficients <- ((matTranspose%*%theMatrix)^-1)%*%matTranspose%*%y
+# print(regressionsCoefficients)
+
+regressionsCoefficients <- solve((t(X) %*% X) , (t(X) %*% y)) #((matTranspose%*%theMatrix)^-1)%*%matTranspose%*%y
+print(X)
 print(regressionsCoefficients)
 #---------------------------------------------Fitted Values-----------------------------------------
-fittedValue <- theMatrix %*% regressionsCoefficients
+fittedValue <- X %*% regressionsCoefficients
+print(fittedValue)
 #---------------------------------------------- The residuals --------------------------------------
 theResiduals <- y - fittedValue
+print(theResiduals)
 #----------------------------------------------The Degree of Freedom--------------------------------
-p <- regressionsCoefficients[1,]
-print (p)
+# p <- regressionsCoefficients[1,]
+# print (p)
 
-degreeOfFreedom <- nrow(theMatrix) - ncol(theMatrix)
+degreeOfFreedom <- nrow(X) - ncol(X)
 print(degreeOfFreedom)
 
 #----------------------------------------------The Residual Variance--------------------------------
@@ -38,10 +54,12 @@ print(residualVariance)
 
 #----------------------------------------------The Variance Of The Regression Coefficients----------
 
-varianceOfRegressionCoefficients <- residualVariance * ((matTranspose %*% theMatrix) ^ -1)
+varianceOfRegressionCoefficients <- residualVariance * solve(t(X) %*% X)
 print(varianceOfRegressionCoefficients)
 
 #----------------------------------------------The t-values For Each Coefficient--------------------
 
-# tValueForEachCoefficient <- regressionsCoefficients / sqrt(varianceOfRegressionCoefficients)
-# print(tValueForEachCoefficient)
+tValueForEachCoefficient <- regressionsCoefficients / sqrt(varianceOfRegressionCoefficients)
+print(tValueForEachCoefficient)
+print(regressionsCoefficients)
+print(sqrt(varianceOfRegressionCoefficients))
