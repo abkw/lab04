2dcd9cf52e1458eaccd429bc21f67d0d530314cc
Mohammed Bakheet <m.yousify@gmail.com>
2019-09-29 14:42:06 +0200

    rebuilding vignette

--- doc/lab04-Vingnette.R
+++ doc/lab04-Vingnette.R
@@ -7,3 +7,29 @@ knitr::opts_chunk$set(
 ## ----setup---------------------------------------------------------------
 library(lab04)
 
+## ----two-----------------------------------------------------------------
+item <- linreg$new(formula = Petal.Length~Species, data = iris)
+item <- linreg(formula = Petal.Length~Species, data = iris)
+item$print()
+
+## ----plot----------------------------------------------------------------
+item$plot()
+
+## ----resid---------------------------------------------------------------
+item$resid()
+
+## ----pred----------------------------------------------------------------
+item$pred()
+
+## ----coef----------------------------------------------------------------
+item$coef()
+
+## ----summary-------------------------------------------------------------
+item$summary()
+
+## ----one, include=TRUE---------------------------------------------------
+item <- linreg(formula = Petal.Length~Species, data = iris)
+item$print()
+item$summary()
+
+
